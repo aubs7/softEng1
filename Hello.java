7d8236541ef5689a67705d1fab0738c0ca6cfcd1
Aubrey Min Lasala <aubrey.min9@gmail.com>
2023-09-01 18:12:09 +0800

    Adding some changes to Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
   public static void main(String[] args){
     System.out.println("Hello, Software Engineering 1");
     System.out.println("Student name: LASALA, Aubrey Min");
+ System.out.println("That is why developers are highly-paid!");
   }
 }
