dbad55942da3853a8ca1287e86c0d2758f82b4db
Aubrey Min Lasala <aubrey.min9@gmail.com>
2023-09-01 18:53:03 +0800

    Haha

--- Hello.java
+++ Hello.java
@@ -2,6 +2,5 @@ public class Hello {
   public static void main(String[] args){
     System.out.println("Hello, Software Engineering 1");
     System.out.println("Student name: LASALA, Aubrey Min");
-    System.out.println ("Complicated world of SoftDev that is why it pays much!");
   }
 }
