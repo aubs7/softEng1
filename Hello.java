3b66f8195d2ed00fa9270226c1dfa6c272a17b05
Aubrey Min Lasala <aubrey.min9@gmail.com>
2023-09-01 18:33:34 +0800

    Added MyNewFile.txt

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
   public static void main(String[] args){
     System.out.println("Hello, Software Engineering 1");
     System.out.println("Student name: LASALA, Aubrey Min");
- System.out.println("That is why developers are highly-paid!");
+    System.out.println("That is why developers are highly-paid!");
+    System.out.println ("Complicated world of SoftDev that is why it pays much!");
   }
 }
